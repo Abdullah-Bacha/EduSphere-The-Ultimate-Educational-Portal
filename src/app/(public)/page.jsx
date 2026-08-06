e31fceac7903fd6dc12e5e7eb701d7326310735b
Abdullah Bacha <bacha141998@gmail.com>
2026-08-06 15:42:01 +0500

    fix: disable pre-rendering on home page to prevent MongoDB connection timeout during build

--- src/app/(public)/page.jsx
+++ src/app/(public)/page.jsx
@@ -10,6 +10,8 @@ import FeaturedCourses from "../components/home/FeaturedCourses";
 import FeaturedTeachers from "../components/home/FeaturedTeachers";
 import FAQSection from "../components/home/FAQSection";
 
+export const dynamic = 'force-dynamic';
+
 export const metadata = {
   title: "Home",
   description:
